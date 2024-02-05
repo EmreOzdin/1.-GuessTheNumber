2b47772cdb51a8f4679f5a11d6e72295cde34a72
Emre Özdin <mrzdnboun@gmail.com>
2024-02-05 16:36:33 +0300

    Refactor code for the React Native app: - Added state management for user input and game screen. - Added functions to handle user input, reset input, and confirm input. - Added error handling for invalid user input. - Added a background image and styles for the app.

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { View, StyleSheet } from "react-native";
-import StartGameScreen from "./screens/StartGameScreen";
+import { useState } from "react";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
+
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/backgroundImage.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        {screen}
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,5 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,10 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton({ children }) {
+function PrimaryButton({ children, onPress }) {
   function pressHandler() {
-    console.log("pressed");
+    onPress();
   }
+
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
@@ -12,7 +13,7 @@ function PrimaryButton({ children }) {
             ? [styles.buttonInnerContainer, styles.pressed]
             : styles.buttonInnerContainer
         }
-        onPress={pressHandler}
+        onPress={onPress}
         android_ripple={{ color: "#640233" }}
       >
         <Text style={[styles.buttonText]}>{children}</Text>
