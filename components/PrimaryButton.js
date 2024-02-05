b799d0f5d4e27fa0805967c56cf77d9e1e0f6de0
Emre Özdin <mrzdnboun@gmail.com>
2024-02-05 12:32:58 +0300

    rootscreen BG added

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -7,11 +7,15 @@ function PrimaryButton({ children }) {
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
-        style={styles.buttonInnerContainer}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
         onPress={pressHandler}
         android_ripple={{ color: "#640233" }}
       >
-        <Text style={styles.buttonText}>{children}</Text>
+        <Text style={[styles.buttonText]}>{children}</Text>
       </Pressable>
     </View>
   );
@@ -27,7 +31,6 @@ const styles = StyleSheet.create({
   },
   buttonInnerContainer: {
     backgroundColor: "#72063c",
-
     paddingVertical: 8,
     paddingHorizontal: 16,
     margin: 4,
@@ -36,5 +39,8 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
   },
 });
